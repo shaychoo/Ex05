1571f8bb2cd7a376e62380de9723aac2e89c7c9f
Shay Trjmn <shaytrjmn@gmail.com>
2015-01-10 20:50:40 +0200

    שאני ארגיש שעשיתי משהו
    הוספתי את הלוח משחק ורפרנס ללוגיקה

---
+++ A15 Ex05 Shay 200779072 Nethanel 301294898/TicTacToeGameFormView/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToeGameFormView
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            TicTacToeGameBoard ticTacToe = new TicTacToeGameBoard();
+            ticTacToe.ShowDialog();
+        }
+    }
+}
